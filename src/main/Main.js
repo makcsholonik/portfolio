c9fddc944f315e98a89817e6620d2cab1d3897eb
makcsholonik <makcsholonik@gmail.com>
2021-06-24 00:12:24 +0300

    stage II

--- src/main/Main.js
+++ src/main/Main.js
@@ -5,7 +5,7 @@ import styleContainer from './../common/styles/Container.module.css'
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={styleContainer.container }>
                 <div className={style.text}>
                     <span>Hi There</span>
                     <h1>I am Maxim</h1>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -8,8 +8,11 @@ export const Project = (props) => {
             <div className={style.imageContainer}>
                 <a className={style.imageButton} href="#">Смотреть</a>
             </div>
-            <h3 className={style.projectTitle}>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}>{props.title}</h3>
+                <span className={style.description}>{props.description}</span>
+            </div>
+
         </div>
     );
 }
