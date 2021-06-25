b1686eefa2a64c81d79bdd8491059d047e87a54f
makcsholonik <makcsholonik@gmail.com>
2021-06-25 23:09:54 +0300

    stage II

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,20 +1,30 @@
-import React from "react";
-import styleContainer from './../common/styles/Container.module.css';
-import style from './../projects/Projects.module.css';
-import { Project } from "./project/Project";
-import { Title } from "../common/components/title/Title";
+import React from 'react'
+import styleContainer from './../common/styles/Container.module.css'
+import style from './../projects/Projects.module.css'
+import { Project } from './project/Project'
+import { Title } from '../common/components/title/Title'
+import socialImg from './../assets/image/socialNetwork.png'
+import todoImg from './../assets/image/todolist.png'
 
 
 export const Projects = () => {
+    const socialNetwork = {
+        backgroundImage: `url(${socialImg})`,
+    };
+    const todolist = {
+        backgroundImage: `url(${todoImg})`,
+    };
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title title={"Projects"}/>
                 <div className={style.projects}>
                     <Project
+                        style={socialNetwork}
                         title={"social network"}
                         description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}/>
                     <Project
+                        style={todolist}
                         title={"todolist"}
                         description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}/>
                 </div>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,18 +1,16 @@
 import React from "react";
 import style from './../project/Project.module.css'
 
-
 export const Project = (props) => {
     return (
         <div className={style.project}>
-            <div className={style.imageContainer}>
+            <div className={style.imageContainer} style={props.style}>
                 <a className={style.imageButton} href="#">Смотреть</a>
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
                 <span className={style.description}>{props.description}</span>
             </div>
-
         </div>
     );
 }
