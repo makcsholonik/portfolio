56d81f71c1b5e70418b7517b106697cbb7e0d1c3
makcsholonik <makcsholonik@gmail.com>
2022-01-13 00:05:33 +0300

    stage II

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -17,16 +17,16 @@ export const Projects = () => {
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <Title title={"Projects"}/>
+                <Title title={'Projects'}/>
                 <div className={style.projects}>
                     <Project
                         style={socialNetwork}
-                        title={"social network"}
-                        description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}/>
+                        title={'social network'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}/>
                     <Project
                         style={todolist}
-                        title={"todolist"}
-                        description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}/>
+                        title={'todolist'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}/>
                 </div>
             </div>
         </div>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 import style from './Project.module.scss'
+import { configure } from '@testing-library/react';
 
 export const Project = (props) => {
     return (
         <div className={style.project}>
             <div className={style.imageContainer} style={props.style}>
-                <a className={style.imageButton}>Смотреть</a>
+                <a className={style.imageButton}></a>
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
