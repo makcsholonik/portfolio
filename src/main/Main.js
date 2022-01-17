fc9e8f051dfa0053a98061b85ee300ae95ad8e9f
makcsholonik <makcsholonik@gmail.com>
2022-01-17 21:57:45 +0300

    stage II

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.scss'
+import maxImg from './../assets/image/maxim.png'
 
 export const Main = () => {
+    const maxim = {
+        backgroundImage: `url(${maxImg})`
+    };
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
                     <span>Hi There</span>
-                    <h1>I am Maxim</h1>
+                    <h1>I am <span>Maxim Sholonik</span></h1>
                     <p>Front-end developer</p>
                 </div>
-                <div className={style.photo}>
-                </div>
+                <div className={style.photo} style={maxim}></div>
             </div>
         </div>
     );
