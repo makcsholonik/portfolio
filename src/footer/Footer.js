35e111c6a55375b07f4ae92ea1aab4c2cb62fa4c
makcsholonik <makcsholonik@gmail.com>
2022-01-19 22:02:49 +0300

    stage II (Footer)

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,56 +1,40 @@
 import React from 'react';
 import style from './Footer.module.scss';
-import styleContainer from '../common/styles/Container.module.scss';
+import telegramIcon from '../assets/images/telegram.svg';
+import facebookIcon from '../assets/images/facebook.svg';
+import linkedinIcon from '../assets/images/linkedin.svg';
+import vkIcon from '../assets/images/vk.svg';
+import { Title } from '../common/components/title/Title';
 
 export const Footer = () => {
     return (
-        <div className={style.footerBlock}>
-            <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Maxim</h2>
-                <div className={style.network}>
-                    <div>facebook</div>
-                    <div>twitter</div>
-                    <div>linkedin</div>
+        <div className={style.footer}>
+            <div className={style.container}>
+                <Title title={'Maxim Sholonik'}/>
+                <div className={style.socialIcons}>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            <img src={telegramIcon} alt=""/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            <img src={facebookIcon} alt=""/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            <img src={linkedinIcon} alt=""/>
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            <img src={vkIcon} alt=""/>
+                        </a>
+                    </div>
                 </div>
-                <div className={style.copyright}>
-                    <img src="#" alt="image"/>
-                    <div>2021 Все права защищены</div>
-                </div>
+                <span className={style.copyright}>2020 All Rights Reserved.</span>
             </div>
         </div>
     )
 }
-
-
-// function Footer() {
-//     return (
-//         <div className={styles.footer}>
-//             <div className={styles.container} >
-//                 <Title text={"Svetlana Dyablo"}/>
-//                 <div className={styles.socialIcons}>
-//                     <div className={styles.socialIcon}>
-//                         <a href="">
-//                             <img src={telegramIcon} alt=""/>
-//                         </a>
-//                     </div>
-//                     <div className={styles.socialIcon}>
-//                         <a href="">
-//                             <img src={facebookIcon} alt=""/>
-//                         </a>
-//                     </div>
-//                     <div className={styles.socialIcon}>
-//                         <a href="">
-//                             <img src={likedinIcon} alt=""/>
-//                         </a>
-//                     </div>
-//                     <div className={styles.socialIcon}>
-//                         <a href="">
-//                             <img src={vkIcon} alt=""/>
-//                         </a>
-//                     </div>
-//                 </div>
-//                 <span className={styles.copyright}>2020 All Rights Reserved.</span>
-//             </div>
-//         </div>
-//     );
-// }
