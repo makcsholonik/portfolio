bf97977c4c93ababcb2d7e1af328b42794d6fa79
makcsholonik <makcsholonik@gmail.com>
2022-01-19 21:15:34 +0300

    stage II (Skills, Skill & Title)

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,48 +1,39 @@
-import React from "react";
+import React from 'react';
 import style from './Skills.module.scss';
-import styleContainer from '../common/styles/Container.module.scss'
-import { Skill } from "./skill/Skill";
-import { Title } from "../common/components/title/Title";
+import { Skill } from './skill/Skill';
+import { Title } from '../common/components/title/Title';
+import reactIcon from '../assets/images/react-icon.svg'
+import jsIcon from '../assets/images/js-icon.svg'
+import htmlIcon from '../assets/images/html-icon.svg'
 
 
 export const Skills = () => {
+    let react = reactIcon;
+    let js = jsIcon;
+    let html = htmlIcon;
+
     return (
         <div className={style.skillsBlock}>
-            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <Title title={"Skills"}/>
+            <div className={style.container}>
+                <Title title={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill title={"HTML"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
-                    <Skill title={"CSS"}
-                           description={"Ipsum laborum laudantium molestiae? Autem beatae blanditiis delectus est ex molestiae nam numquam quibusdam vero voluptates"}/>
-                    <Skill title={"HTML"} description={"Adipisci eum incidunt porro tempore vitae."}/>
+                    <Skill
+                        title={'HTML & CSS'}
+                        icon={html}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
+                    />
+                    <Skill
+                        title={'JS'}
+                        icon={js}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
+                    />
+                    <Skill
+                        title={'React'}
+                        icon={react}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
+                    />
                 </div>
             </div>
         </div>
     );
 }
-
-
-// function Skills() {
-//     let react = reactIcon;
-//     let js = jsIcon;
-//     let html = htmlIcon;
-//
-//     return (
-//         <div className={styles.skillsBlock}>
-//             <div className={styles.container}>
-//                 <Title text={"Skills"}/>
-//                 <div className={styles.skills}>
-//                     <Skill title={"Js"}
-//                            icon={js}
-//                            description={"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."}/>
-//                     <Skill title={"html & Css"}
-//                            icon={html}
-//                            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, "}/>
-//                     <Skill title={"React"}
-//                            icon={react}
-//                            description={"ut labore et dolore magna aliqua Ut."}/>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -4,8 +4,10 @@ import style from './Skill.module.scss';
 export const Skill = (props) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
-            <h3>{props.title}</h3>
+            <div className={style.icon}>
+                <img src={props.icon} alt=""/>
+            </div>
+            <h3 className={style.skillTitle}>{props.title}</h3>
             <span className={style.description}>
                 {props.description}
             </span>
