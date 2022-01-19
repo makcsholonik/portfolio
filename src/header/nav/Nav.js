ba3170f4dfded196da295306a08f3f118536ce07
makcsholonik <makcsholonik@gmail.com>
2022-01-19 19:15:35 +0300

    stage II (Main)

--- src/header/nav/Nav.js
+++ src/header/nav/Nav.js
@@ -1,25 +1,13 @@
-import React from "react";
+import React from 'react';
 import style from './Nav.module.scss';
 
 export const Nav = () => {
     return (
         <div className={style.nav}>
-            <a href="">Главная</a>
-            <a href="">Скиллы</a>
-            <a href="">Проекты</a>
-            <a href="">Контакты</a>
+            <a href="">Main</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+            <a href="">Contacts</a>
         </div>
     );
 }
-
-
-// function Nav() {
-//     return (
-//         <div className={style.nav}>
-//             <a href="">Main</a>
-//             <a href="">Skills</a>
-//             <a href="">Projects</a>
-//             <a href="">Contacts</a>
-//         </div>
-//     );
-// }
--- src/main/Main.js
+++ src/main/Main.js
@@ -17,24 +17,3 @@ export const Main = () => {
         </div>
     );
 }
-
-
-//function Main() {
-// //     return (
-// //         <div className={styles.mainBlock}>
-// //             <div className={styles.container}>
-// //                 <div className={styles.greeting}>
-// //                     <span>Hi There</span>
-// //                     <span>I am Svetlana <span>Dyablo</span></span>
-// //                     <h1>Frontend Developer.</h1>
-// //                 </div>
-// //                 <div className={styles.photo}>
-// //                     <div className={styles.image}>
-// //
-// //                     </div>
-// //                 </div>
-// //
-// //             </div>
-// //         </div>
-// //     );
-// // }
