77f37dec48615eb1d0245fe5e842ec775db02f36
makcsholonik <makcsholonik@gmail.com>
2022-01-25 21:46:34 +0300

    stage III (Fade)

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import style from './Main.module.scss';
+import Fade from 'react-reveal/Fade';
 
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
             <div className={style.container}>
-                <div className={style.greeting}>
-                    <span>Hi There</span>
-                    <h1>I am <span>Maxim Sholonik</span></h1>
-                    <p>Front-end developer</p>
-                </div>
-                <div className={style.photo}>
-                    <div className={style.image}></div>
-                </div>
+                <Fade left>
+                    <div className={style.greeting}>
+                        <span>Hi There</span>
+                        <h1>I am <span>Maxim Sholonik</span></h1>
+                        <p>Front-end developer</p>
+                    </div>
+                </Fade>
+                <Fade right>
+                    <div className={style.photo}>
+                        <div className={style.image}></div>
+                    </div>
+                </Fade>
             </div>
         </div>
     );
